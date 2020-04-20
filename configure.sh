75089ef6c8e0fcbfeb63bd13150205ac2c977bfc
sprindjack <sprindjack@hotmail.com>
2020-04-20 13:01:14 +0800

    Create configure.sh

---
+++ configure.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Download
+wget https://github.com/tindy2013/subconverter/releases/latest/download/subconverter_linux64.tar.gz
+tar -xvf subconverter_linux64.tar.gz
+rm subconverter_linux64.tar.gz
+chmod +x *
+
+# Run
+./subconverter
