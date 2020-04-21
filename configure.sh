be4f7ed2329b659064db687a155592968599cd5d
sprindjack <sprindjack@hotmail.com>
2020-04-21 14:15:39 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Download
-wget https://github.com/sprindjack/subconverter/releases/latest/download/subconverter_linux64.tar.gz
+wget https://github.com/tindy2013/subconverter/releases/latest/download/subconverter_linux64.tar.gz
 tar -xvf subconverter_linux64.tar.gz
 rm subconverter_linux64.tar.gz
 cd /subconverter
