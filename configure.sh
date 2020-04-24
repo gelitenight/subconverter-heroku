bf60bcffba9af465de63ea4a01697b6d40f37334
sprindjack <sprindjack@hotmail.com>
2020-04-24 16:37:34 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -7,12 +7,17 @@ rm subconverter_linux64.tar.gz
 cd subconverter
 chmod +x *
 
-# Configure your subdomain
+# DIY Part
+# 1. Configure subdomain
 sed -i "s/(http://127.0.0.1:25500)/(https://127.0.0.1:25500)/g" pref-new.yml
 sed -i "s/(http://127.0.0.1:25500)/(https://127.0.0.1:25500)/g" pref.ini
 abc=$Domain
 sed -i "s/127.0.0.1:25500/${abc}/g" pref-new.yml
 sed -i "s/127.0.0.1:25500/${abc}/g" pref.ini
+
+# 2. Set expire time to infinite, manually update if the sub config changed
+sed -i 's/86400/0/g' pref-new.yml
+sed -i 's/86400/0/g' pref.ini
 
 # Run
 ./subconverter
